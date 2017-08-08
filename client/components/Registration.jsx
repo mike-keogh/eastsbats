987e320d8ed7256b795c2dd8e1696bbbf3f00fb7
Michael Keogh <brookemossburn@gmail.com>
2017-08-09 08:36:05 +1200

    Form on registration page, not sending data

--- client/components/Registration.jsx
+++ client/components/Registration.jsx
@@ -11,9 +11,23 @@ export default class Registration extends React.Component {
   render() {
     return (
       <div className="addNewPlayer">
-        <form name="newPlayer" >
+        <form >
+        <input name="name" placeholder="name" type="text"/><br/>
+        <input name="born" placeholder="born" type="text" /><br/>
+        <input name="age" placeholder="age" type="text" /><br/>
+        <input name="teams" placeholder="Major Teams" type="text" /><br/>
+        <input name="playing_role" placeholder="Playing Role" type="text" /><br/>
+        <select name="batting_style">
+          <option selected disabled>Batting Style</option>
+          <option>Right-hand Bat</option>
+          <option>Left-hand Bat</option>
+        </select><br/>
 
-        </form>
+        <input name="bowling_style" placeholder="Bowling Style" type="text" /><br/>
+        <input type="submit" />
+
+      </form>
+
       </div>
     )
   }
