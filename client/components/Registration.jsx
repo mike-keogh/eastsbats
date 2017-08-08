991d9224f1943d5e0466f36242043d6026ae7bd4
Michael Keogh <brookemossburn@gmail.com>
2017-08-08 13:36:12 +1200

    register component started

---
+++ client/components/Registration.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default class Registration extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      newPlayer = []
+    }
+  }
+
+  render() {
+    return (
+      <div className="addNewPlayer">
+        <form name="newPlayer" >
+          
+        </form>
+      </div>
+    )
+  }
+}
